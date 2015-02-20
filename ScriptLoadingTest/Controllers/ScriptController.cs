dc124f8a0b33fd1a6b1ea5e0670744c58b165f29
pradudev <pradeepchouhan1986@gmail.com>
2015-02-20 14:20:43 +0530

    Initial commit

---
+++ ScriptLoadingTest/Controllers/ScriptController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScriptLoadingTest.Controllers
+{
+    public class ScriptController : Controller
+    {
+        public ActionResult File([Bind(Prefix = "id")] int fileNum)
+        {
+            System.Threading.Thread.Sleep(fileNum * 1000);
+
+            return Content("console.log('File" + fileNum + " loaded')", "text/javascript");
+        }
+    }
+}
---
+++ ScriptLoadingTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScriptLoadingTest.MvcApplication" Language="C#" %>
